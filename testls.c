aa01776654af95a5fb01247e213d54ad2c845e2e
henriquejdc <riquejdc@gmail.com>
2017-05-19 17:19:30 -0300

    Arquivo de teste

--- testls.c
+++ testls.c
@@ -5,13 +5,12 @@
 #include "user.h"       //functios like printf and syscalls
 
 #define STDOUT 1        
-#define PROCESS 40
+#define PROCESS 30
 #define T 112345678
 
 void tempo(){ //função para gastar tempo 
-    int i,j;
-    i=j=T;
-    while(j--){}
+    int i;
+    i=T;
     while(i--){}
 }
 
@@ -19,14 +18,12 @@ int main(){
     int pid;
     int i;
     for (i=0;i<PROCESS;i++){ //cria processos filhos
-        if(i<(PROCESS/4)){//1/4 das entradas
-            pid=fork(100); 
-        }else if(i>=(PROCESS/4) &&  i<(2*(PROCESS/4))){ //1/2 das entradas
-            pid=fork(0);
-        }else if(i>=(2*PROCESS/4) &&  i<(3*(PROCESS/4))){ //3/4 das entradas
-            pid=fork(100); 
-        }else{//ultimas entradas
-            pid=fork(-10); 
+        if(i<(PROCESS/3)){//fork do int 1 
+            pid=fork(1); 
+        }else if(i>=(PROCESS/3) &&  i<(2*(PROCESS/3))    ){ //fork do int 4
+            pid=fork(4);
+        }else{
+            pid=fork(8); //fork do int  8
         }if(pid == 0){
             tempo(); //gasta tempo
             exit();
