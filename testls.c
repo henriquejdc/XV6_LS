e80fb2936c272630c371e4580b84418fb4183d9f
henriquejdc <riquejdc@gmail.com>
2017-05-19 17:10:16 -0300

    Arquivo teste

--- testls.c
+++ testls.c
@@ -19,14 +19,14 @@ int main(){
     int pid;
     int i;
     for (i=0;i<PROCESS;i++){ //cria processos filhos
-        if(i<(PROCESS/4)){//fork do int 1 
+        if(i<(PROCESS/4)){//1/4 das entradas
             pid=fork(100); 
-        }else if(i>=(PROCESS/4) &&  i<(2*(PROCESS/4))){ //fork do int 4
+        }else if(i>=(PROCESS/4) &&  i<(2*(PROCESS/4))){ //1/2 das entradas
             pid=fork(0);
-        }else if(i>=(2*PROCESS/4) &&  i<(3*(PROCESS/4))){
-            pid=fork(100); //fork do int  8
-        }else{
-            pid=fork(-10); //fork do int  8
+        }else if(i>=(2*PROCESS/4) &&  i<(3*(PROCESS/4))){ //3/4 das entradas
+            pid=fork(100); 
+        }else{//ultimas entradas
+            pid=fork(-10); 
         }if(pid == 0){
             tempo(); //gasta tempo
             exit();
